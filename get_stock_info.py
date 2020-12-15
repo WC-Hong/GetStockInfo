82c6167d2950d7a7ff10ecbb43a632a5113eae4c
Aries <d7404001@gmail.com>
2020-12-15 18:23:14 +0800

    Add new function
    The program can looping execution and sleep 5 minutes every cycle.

--- get_stock_info.py
+++ get_stock_info.py
@@ -1,4 +1,11 @@
+import time
 import requests
+from bs4 import BeautifulSoup
 
-resquest = requests.get("https://s.yimg.com/nb/tw_stock_frontend/scripts/StxChart/StxChart.9d11dfe155.html?sid=2610")
 
+    # get html request
+    # request = requests.get("https://s.yimg.com/nb/tw_stock_frontend/scripts/StxChart/StxChart.9d11dfe155.html?sid=2610")
+    # soup = BeautifulSoup(request.text, 'html.parser')  # put html source code into html parser
+    # content = soup.body
+while True:
+    time.sleep(300)  # sleep 5 minute
