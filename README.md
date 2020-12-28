600ec7d9fcdbc68e1126e71aa9f852586c26c921
Aries <d7404001@gmail.com>
2020-12-28 15:04:54 +0800

    Add annotation
    Add annotation in code

---
+++ README.md
@@ -0,0 +1,10 @@
+# 這是有關於抓取股票消息的程式碼
+## 開發環境
+* Ubuntu 18.04
+* Python 3.6.9
+* pip 套件: requests、pymysql
+## 如何執行?
+在指令列輸入
+```
+$ python3 get_stock_info.py <股票代號> <抓取時間>
+```
--- get_stock_info.py
+++ get_stock_info.py
@@ -4,14 +4,18 @@ import sys
 
 # get html request
 url = "https://api.finmindtrade.com/api/v3/data"
+
+# this is parameter of get of http method
+# the fllowing parameters are too complex to explain, try referring https://finmind.github.io/tutor/TaiwanMarket/DataList/
 parameter = {
     "dataset": "TaiwanStockPrice",
     "stock_id": sys.argv[1],
 }
 parameter["date"] = sys.argv[2]
 
-data = requests.get(url, params=parameter).json()["data"]
+data = requests.get(url, params=parameter).json()["data"]  # get stock data from get request
 
+# the fllowing arguments are about mysql configurations
 db_settings = {
     "host": "127.0.0.1",
     "port": 3306,
@@ -22,10 +26,11 @@ db_settings = {
 }
 
 try:
-    conn = pymysql.connect(**db_settings)
+    conn = pymysql.connect(**db_settings)  # connect to mysql
+
     with conn.cursor() as cursor:
         command = "INSERT INTO <table>(columns) VALUES (values)"
         cursor.execute(command, ())  # secand parameter is columns value
-        conn.commit()
+        conn.commit()  # let mysql execute command
 except Exception as e:
     print(e)
